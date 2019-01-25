e675618bf0154a9d8e8d2cae831f2105e2eb206a
andytanghr <30758932+andytanghr@users.noreply.github.com>
2019-01-25 16:25:10 -0600

    setting up router

--- reactivebpm/src/index.js
+++ reactivebpm/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from './routes';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
