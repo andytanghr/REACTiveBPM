69e898a6b248479e05a23ccb3900c5fc0199fec7
andytanghr <30758932+andytanghr@users.noreply.github.com>
2019-01-25 17:57:59 -0600

    started Metronome

---
+++ reactivebpm/src/components/Metronome.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import './Metronome.css'
+
+class Metronome extends Component {
+  render() {
+    let bpm = 100;
+    let playing = false;
+
+
+
+
+    return (
+      <div className='metronome'>
+        <h1>I AM A TICK TOCK</h1>
+    
+      </div>
+    );
+  };
+}
+
+export default Metronome;
