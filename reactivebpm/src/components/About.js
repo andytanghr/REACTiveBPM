aedaa1a320df698ff1585c4001d7f2664d1f4cee
andytanghr <30758932+andytanghr@users.noreply.github.com>
2019-01-25 17:47:54 -0600

    routes implemented

---
+++ reactivebpm/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='jumbotron'>
+      <h1>Hello, world</h1>
+      <p>This is a metronome and beat detector app built with React.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ reactivebpm/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li><Link to='/About'>About</Link></li>
+        <li><Link to='/Metronome'>Metronome</Link></li>
+        <li><Link to='/BeatDetector'>Beat Detector</Link></li>
+      </ul>
+      <hr/>
+
+    </div>
+  )
+}
+
+export default NavBar;
