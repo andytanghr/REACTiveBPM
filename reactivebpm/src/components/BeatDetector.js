9d35cb353c2d2546c56dc1a2ce3c5501202dc455
andytanghr <30758932+andytanghr@users.noreply.github.com>
2019-01-26 15:45:20 -0600

    started beatDetector feature

---
+++ reactivebpm/src/components/BeatDetector.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import './beatDetector.css'
+
+class BeatDetector extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      bpm: 0
+    }
+  }
+
+
+
+
+
+  render() {
+
+    return(
+      <div className='beatDetector'>
+        
+
+
+
+      </div>
+    );
+
+
+
+  };
+}
+
+export default BeatDetector;
